6125ccd1071b8fcace9999db1160fa83b7faee03
Miliczki Áron_10B <maron33352@gmail.com>
2022-11-15 09:25:39 +0100

    changes 1

---
+++ data.py
@@ -0,0 +1,27 @@
+file = 'data.csv'
+Név = []
+Pin = []
+Számlaszám = []
+Egyenleg = []
+címsor = ''
+
+def betölt():
+    with open(file,'r', encoding='utf-8') as forras:
+        global címsor
+        címsor = forras.readline()
+        for row in forras:
+            halmaz = row.split(';')
+            Név.append(halmaz[0])
+            Pin.append(halmaz[1])
+            Számlaszám.append(halmaz[2])
+            Egyenleg.append(halmaz[3].strip())
+
+def Mentés():
+    with open(file, 'w', encoding='utf-8') as cel:
+        cel.write(címsor)
+        for i in range(len(Név)):
+            cel.write(f'{Név[i]};{Pin[i]};{Számlaszám[i]};{Egyenleg[i]}\n')
+        Név.clear()
+        Pin.clear()
+        Számlaszám.clear()
+        Egyenleg.clear()
---
+++ main.py
@@ -0,0 +1,5 @@
+from functions import *
+from data import *
+
+betölt()
+Mentés()
